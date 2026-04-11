db44691d3d6c4e63c01fafcc689200377c228248
Simone Corrà <132148950+Corra03@users.noreply.github.com>
2026-04-11 22:44:08 +0200

    Add files via upload
    add crypto module and tests

---
+++ Cipher-diary/crypto.py
@@ -0,0 +1,34 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+def derive_key(psw, salt):
+    psw = psw.encode("utf-8")
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1_200_000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(psw))
+    return key
+
+def encrypt(text, psw):
+    salt = os.urandom(16)
+    key = derive_key(psw, salt)
+    f = Fernet(key)
+    token = f.encrypt(text.encode("utf-8"))
+    return salt + token
+
+def decrypt(data, psw):
+    salt, token = data[:16], data[16:]
+    key = derive_key(psw, salt)
+    f = Fernet(key)
+    return(f.decrypt(token))
+
+
+if __name__ == "__main__":
+    print("hello world")
